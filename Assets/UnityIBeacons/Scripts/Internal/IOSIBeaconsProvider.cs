69c3f7b8f0b5356451d22c1955635e74b0d15afb
Alberto Gonzalez Perez <algonzal@uji.es>
2019-10-28 20:54:18 +0100

    Fix a bug that impeded IL2CPP Android builds

--- Assets/UnityIBeacons/Scripts/Internal/IOSIBeaconsProvider.cs
+++ Assets/UnityIBeacons/Scripts/Internal/IOSIBeaconsProvider.cs
@@ -4,6 +4,7 @@ namespace IBeacons.Internal
 {
     public class IOSIBeaconsProvider : IBeaconsProvider
     {
+#if UNITY_IOS
         [DllImport("__Internal")]
         private static extern void _uib_initialize(string appUID);
 
@@ -12,20 +13,27 @@ namespace IBeacons.Internal
 
         [DllImport("__Internal")]
         private static extern void _uib_stopScan();
+#endif
 
         public IOSIBeaconsProvider(string appUUID)
         {
+#if UNITY_IOS
             _uib_initialize(appUUID);
+#endif
         }
 
         public void StartScan()
         {
+#if UNITY_IOS
             _uib_startScan();
+#endif
         }
 
         public void StopScan()
         {
+#if UNITY_IOS
             _uib_stopScan();
+#endif
         }
     }
 }
